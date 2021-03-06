2696d3724e9a182638c0b976ca749e38d3ea4faa
Roger Takeshita <roger.takeshita@gmail.com>
2021-03-05 21:54:57 -0500

    style: Should update colors

--- src/print.js
+++ src/print.js
@@ -8,47 +8,47 @@ const printFileStatus = (files) => {
     }
     if (files.commitAheadMsg) {
         console.log(
-            chalk`    {${rgb.WHT}.${rgbBG.GND}  C } {${rgb.GND}.bold ${files.commitAheadMsg}}`,
+            chalk`    {${rgb.WHT}.${rgbBG.GND2}.bold  C } {${rgb.GND2}.bold ${files.commitAheadMsg}}`,
         );
     }
     if (files.commitBehindMsg) {
         console.log(
-            chalk`    {${rgb.WHT}.${rgbBG.BLD}  B } {${rgb.BL}.bold ${files.commitBehindMsg}}`,
+            chalk`    {${rgb.WHT}.${rgbBG.BL}.bold  B } {${rgb.BL}.bold ${files.commitBehindMsg}}`,
         );
     }
     files.stagedNewFiles.forEach((file) =>
         console.log(
-            chalk`    {${rgb.WHT}.${rgbBG.GN}  N } {${rgb.GN} ${file}}`,
+            chalk`    {${rgb.WHT}.${rgbBG.GND}.bold  N } {${rgb.GND} ${file}}`,
         ),
     );
     files.stagedModifiedFiles.forEach((file) =>
         console.log(
-            chalk`    {${rgb.WHT}.${rgbBG.GN}  M } {${rgb.GN} ${file}}`,
+            chalk`    {${rgb.BK}.${rgbBG.GN}.bold  M } {${rgb.GN} ${file}}`,
         ),
     );
     files.stagedDeletedFiles.forEach((file) =>
         console.log(
-            chalk`    {${rgb.WHT}.${rgbBG.GN}  D } {${rgb.GN} ${file}}`,
+            chalk`    {${rgb.WHT}.${rgbBG.GN}.bold  D } {${rgb.GN} ${file}}`,
         ),
     );
     files.notStagedModifiedActiveFiles.forEach((file) =>
         console.log(
-            chalk`    {${rgb.WHT}.${rgbBG.RD}  M } {${rgb.RD} ${file}}`,
+            chalk`    {${rgb.WHT}.${rgbBG.RD}.bold  M } {${rgb.RD} ${file}}`,
         ),
     );
     files.notStagedDeletedFiles.forEach((file) =>
         console.log(
-            chalk`    {${rgb.WHT}.${rgbBG.RD}  D } {${rgb.RD} ${file}}`,
+            chalk`    {${rgb.WHT}.${rgbBG.RD}.bold  D } {${rgb.RD} ${file}}`,
         ),
     );
     files.untrackedFiles.forEach((file) =>
         console.log(
-            chalk`    {${rgb.WHT}.${rgbBG.OG}  ? } {${rgb.OG} ${file}}`,
+            chalk`    {${rgb.WHT}.${rgbBG.OG}.bold  ? } {${rgb.OG} ${file}}`,
         ),
     );
     files.unmergedFiles.forEach((file) =>
         console.log(
-            chalk`    {${rgb.WHT}.${rgbBG.BL}  C } {${rgb.BL} ${file}}`,
+            chalk`    {${rgb.WHT}.${rgbBG.BL}.bold  C } {${rgb.BL} ${file}}`,
         ),
     );
     if (files.commitAheadMsg || files.commitBehindMsg || files.counter) {
